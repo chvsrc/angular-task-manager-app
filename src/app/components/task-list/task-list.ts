f29bf834a5f979d2a2f4718b66d418e8629953ee
Venkat Ch <>
2025-07-10 02:04:53 -0400

    Added pipes (like filter)

--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskService } from '../../services/task';
 import { FormsModule } from '@angular/forms';
+import { StatusPipe } from '../../pipes/status-pipe';
+import { TaskService } from '../../services/task';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, StatusPipe], // ✅ register the pipe
   templateUrl: './task-list.html',
   styleUrls: ['./task-list.css']
 })
@@ -17,7 +18,7 @@ export class TaskListComponent implements OnInit {
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe(data => {
-      this.taskList = data.slice(0, 5); // Limit to 5 for demo
+      this.taskList = data.slice(0, 5);
     });
   }
 }
